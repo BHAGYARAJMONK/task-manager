5ed7de9333f48c4b5d1b7071712799e1666cc94a
BhagyarajKarri <Bhagyaraj@englishmonkapp.com>
2024-12-20 20:15:41 +0530

    Initial commit

---
+++ task-manager-frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <TaskForm />
+  </div>
+</template>
+
+<script>
+import TaskForm from './components/TaskForm.vue';
+
+export default {
+  components: {
+    TaskForm,
+  },
+};
+</script>
